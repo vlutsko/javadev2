4e29d028863c48c6ed871eb05526562b060cdc20
v_lutsko <v_lutsko@ukr.net>
2017-06-19 11:13:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+error_reporting(-1);
+//ini_set('display_errors','on');
+header('Content-Type: text/html; charset=utf-8');
+//Дополнение к уроку: 
+//Возьмите за правило каждый скрипт на PHP начинать с этих строчек!!!
+	
+//require __DIR__ . '/models/photo.php';
+
+//$items = photo_getAll();
+
+//include __DIR__ . '/views/index.php';
+
+phpinfo();
